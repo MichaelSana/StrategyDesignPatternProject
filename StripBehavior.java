b1b6ed06ccefffc78e8adca48fca26cf42a23247
Michael Sana <msana@email.sc.edu>
2021-01-10 17:53:52 -0500

    Added StripBehavior.java

---
+++ StripBehavior.java
@@ -0,0 +1,9 @@
+public class StripBehavior implements DefenceBehavior {
+
+    @Override
+    public String play() {
+        String ret = "strip a ball from runners hand";
+        return ret;
+    }
+    
+}
