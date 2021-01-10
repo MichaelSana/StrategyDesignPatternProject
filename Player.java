4525192f5b4ba30f6a486223c0e055c7c0e424ae
Michael Sana <msana@email.sc.edu>
2021-01-10 17:31:52 -0500

    Added Player.java

---
+++ Player.java
@@ -0,0 +1,10 @@
+package StrategyDesignPatternProject-1;
+
+public class Player{
+    public static void main(String[] args){
+        protected String name;
+        private boolean defence;
+        protected DefenceBehavior defenceBehavior;
+        protected OffenceBehavior offenceBehavior;
+    } 
+}
