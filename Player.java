002c9a4034d6f056ba570a8eebe285034cabcb67
Michael Sana <msana@email.sc.edu>
2021-01-10 17:56:47 -0500

    Updated Player.java to be an abstract class

--- Player.java
+++ Player.java
@@ -1,6 +1,6 @@
 package StrategyDesignPatternProject-1;
 
-public class Player{
+public abstract class Player{
     public static void main(String[] args){
         protected String name;
         private boolean defence;
@@ -20,6 +20,6 @@ public class Player{
 
     }
     public void turnover(){
-        
+
     }
 }
