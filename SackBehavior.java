e660c236fbf52b015e427bad7a34fe5bf0128419
Michael Sana <msana@email.sc.edu>
2021-01-10 17:55:13 -0500

    Added SackBehavior.java

---
+++ SackBehavior.java
@@ -0,0 +1,9 @@
+public class SackBehavior implements DefenceBehavior {
+
+    @Override
+    public String play() {
+        String ret = "sack the quarterback";
+        return ret;
+    }
+    
+}
