bd88bf6fe924f987ce290a0660bcd4f64ae0d41b
Michael Sana <msana@email.sc.edu>
2021-01-11 07:51:46 -0500

    Added Reciever.java and updated Quarterback.java to better fit description

--- Quarterback.java
+++ Quarterback.java
@@ -1,18 +1,15 @@
-import java.util.Random;
+//import java.util.Random;
 public class Quarterback extends Player{
 
     public Quarterback(String name) {
         super(name);
     }
     public void setOffenceBehavior(){
-        Random rand = new Random();
-        int random = rand.nextInt(1);    
-        if (random == 0){
+      
+     
             offenceBehavior = new RunBehavior();
-        }
-        else if(random == 1){
             offenceBehavior = new PassBehavior();
-        }
+       
     }
     public void setDefenceBehavior(){
 
---
+++ Reciever.java
@@ -0,0 +1,13 @@
+public class Reciever extends Player {
+
+    public Reciever(String name) {
+        super(name);
+    }
+    public void setOffenceBehavior(){
+      
+    }
+    public void setDefenceBehavior(){
+
+    }
+    
+}
